5565a2dcb866347ca7ad66af1b6db2733c42245b
Camilo Pardo <contacto@parddo.com>
2021-04-13 19:15:44 -0500

    Configuration initial

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/componentscss/app.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hola camilo</h1>
+
+      <p>dejkfjkjkjkkj</p>
+    </div>
+  );
+}
